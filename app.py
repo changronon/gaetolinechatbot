059d928a58b7cb54c6c15fd5aea1b5ff90b55ce5
changronon <changronon@gmail.com>
2021-09-01 10:08:05 +0800

    updata

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=event.message.text))
-
+            line_reply(event, "測試：")
 
 if __name__ == "__main__":
     app.run()
