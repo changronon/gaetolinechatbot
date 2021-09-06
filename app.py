eeabe7b6cf5840e79f7c1509f032f39ab6e48993
changronon <changronon@gmail.com>
2021-09-06 11:49:12 +0800

    updata

--- app.py
+++ app.py
@@ -1,52 +1,91 @@
 from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+import twder
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
 
 app = Flask(__name__)
 
+msg_tip = """
+歡迎使用 Line 匯率查詢機器人
+---------------------------------
+使用教學：
+1.查詢 幣別 > 回傳該幣別匯率
+2.清單      > 回傳幣別清單
+---------------------------------
+例：查詢 美金
+即回傳新台幣兌換美金匯率
+"""
+
+# Line Bot驗證
 line_bot_api = LineBotApi('g/nQVJOVCYLs/qqQsXuzn3NNgnCAT32o+xoEcOFBhjK+NUEAtqujMPAo+bpTj9xlJ4kny8z750qqUkEld3Q3S+kSurmLTkCV5Ti0vNb4ln6Z8ABnuRva4LA32pj53WVPf88gHzNwSpiiuZeiufEBSAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('1ab2a06316420a3b5ff91b0510618a60')
+# Push訊息到指定的UserID中
+#line_bot_api.push_message("您的Line Bot UserID", TextSendMessage(text="歡迎使用Line 匯率查詢機器人"))
+
+# 台灣銀行可換匯幣別清單
+cur_list = {}
+cur_ch_list = {}
+twch = twder.currency_name_dict()
+cur_list_str = ''
+for i in iter(twch):
+    cur_list[i] = twch[i].split()[0]
+    cur_ch_list[twch[i].split()[0]] = i
+    cur_list_str = cur_list_str + i + twch[i].split()[0] + '\n'
+
+# 查詢輸入幣別即時匯率(可輸入中文或英文)
+def twder_currency(query_cur):
+    # 輸入中文時須轉換成英文
+    if query_cur in cur_list:
+        pass
+    elif query_cur in cur_ch_list:
+        query_cur = cur_ch_list[query_cur]
+    return twder.now(query_cur)
 
 
+# 設定Line Bot回覆訊息
+def createReplyMessge(query_cur):
+    try:
+        replyCheckMessage = (f"您查詢的幣別是：{query_cur}\n時間：%s\n現金買入：%s\n現金賣出：%s\n即期買入：%s\n即期賣出：%s"%twder_currency(query_cur))
+    except:
+        replyCheckMessage = "查無資料，無此幣別!!\n%s"%msg_tip
+    return replyCheckMessage
+
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
-def callback():
+def callback(request):
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
+    print(body)
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-
     return 'OK'
 
+def line_reply(event, msg_text):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextMessage(text=msg_text, type="text")
+        )
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print("event.reply_token:", event.reply_token)
-    print("event.source.user_id:", event.source.user_id)
-    print("event.message.text:", event.message.text)
-    
-    # 加了這行
-    if event.source.user_id != "U9bd03aede5bade3fe5977a7704b71d9d":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text))
-
-
+    if "查詢" in event.message.text or "Query" in event.message.text:
+        try:
+            query_cur = event.message.text.split()[1]
+            # 格式正確,查詢資料
+            line_reply(event, createReplyMessge(query_cur))
+        except:
+            line_reply(event, "輸入錯誤!!\n%s"%msg_tip)
+    elif "清單" in event.message.text or "List" in event.message.text:
+        line_reply(event, cur_list_str)
+    else: # 輸入其他文字則回傳Tip
+        line_reply(event, "輸入錯誤!!\n%s"%msg_tip)
+            
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
+
